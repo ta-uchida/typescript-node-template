1a262950cb1b75bbc29d1ab108c52d7a5958f519
Takayoshi Uchida <takayoshi-uchida@mail.nissan.co.jp>
2020-02-11 09:28:50 +0900

    add proxy sample

--- src/middleware/routes.ts
+++ src/middleware/routes.ts
@@ -1,4 +1,6 @@
 import Express from 'express';
+import { ClientRequest, IncomingMessage, ServerResponse } from 'http';
+import Proxy, { ServerOptions } from 'http-proxy';
 
 import indexService from '../service/index.service';
 
@@ -11,5 +13,27 @@ export default (app: Express.Application) => {
   app.post('/post', (req: Express.Request, res: Express.Response) => {
     console.log(req.body);
     res.status(201).json({});
+  });
+
+  app.post('/proxy/*', (req: Express.Request, res: Express.Response) => {
+    const proxy = Proxy.createProxyServer({});
+    proxy.on(
+      'proxyReq',
+      (proxyReq: ClientRequest, req: IncomingMessage, res: ServerResponse, options: ServerOptions) => {
+        if (req.url) {
+          proxyReq.path = req.url.replace('/proxy', '');
+        }
+        // https://github.com/http-party/node-http-proxy/issues/1142
+        if ((req as Express.Request).body) {
+          const bodyData = JSON.stringify((req as Express.Request).body);
+          proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+          proxyReq.write(bodyData);
+        }
+      },
+    );
+
+    proxy.web(req, res, {
+      target: 'http://localhost:4000',
+    });
   });
 };
